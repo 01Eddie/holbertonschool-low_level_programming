67e7c4aaf3cd2e818b51f83357829d58c32cb65f
01Eddie <jesus.11.97@hotmail.com>
2021-02-04 05:11:21 +0000

    [modified] 6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-int main()
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: 0
+ */
+int main(void)
 {
-	int integerType; 
-	char charType;
-	float floatType; 
-	double doubleType;
-	printf("Size of a char is: %ld\n", sizeof(charType)," byte(s)");
-	printf("Size of an int is: %ld\n", sizeof(integerType)," byte(s)");
-	printf("Size of a long int is: %ld\n", sizeof(doubleType)," byte(s)");
-	printf("Size of a long long is: %ld\n", sizeof(doubleType)," byte(s)");
-	puts("Size of a float is: %ld\n", sizeof(floatType), " byte(s)");
+	printf("Size of a char: %d\n", sizeof(char), " byte(s)");
+	printf("Size of an int: %d\n", sizeof(int), " byte(s)");
+	printf("Size of a long int: %d\n", sizeof(long int), " byte(s)");
+	printf("Size of a long long: %d\n", sizeof(long long), " byte(s)");
+	printf("Size of a float: %d\n", sizeof(float), " byte(s)");
 	return (0);
 }
