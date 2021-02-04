f1eb338991489710565ae1a35198022e4734047f
01Eddie <jesus.11.97@hotmail.com>
2021-02-04 05:44:39 +0000

    [modified] added comment in 5 printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: 0
+ */
 int main(void)
 {
-/*Here printf is continue the char. */
 	printf("with proper grammar, but the outcome is a piece of art,");
-	printf("\n");
 	return (0);
 }
