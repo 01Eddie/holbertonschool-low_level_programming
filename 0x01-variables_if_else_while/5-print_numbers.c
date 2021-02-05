5c79d97d9537d8e84c1986461eda3e90f784d3bc
01Eddie <jesus.11.97@hotmail.com>
2021-02-05 14:33:47 +0000

    [ADD] print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	/* your code goes there */
+	do {
+	printf("%d", n);
+	n++;
+	} while (n != 10);
+	printf("\n");
+	return (0);
+}
+
