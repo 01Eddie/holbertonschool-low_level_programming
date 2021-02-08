55b62118ee257d4a98b879d6c176b65c698513fa
01Eddie <jesus.11.97@hotmail.com>
2021-02-08 05:32:04 +0000

    [ADD] 101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-int main(void) {
-	char x[] = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	write(1,x,56);
+#include <unistd.h>
+
+int main(void) 
+{
+	char x[] = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, x, 59);
 	return 1;
 }
