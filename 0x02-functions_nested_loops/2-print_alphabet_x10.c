14dabf24f9a0b2693012aac8a3f11311593b9922
01Eddie <jesus.11.97@hotmail.com>
2021-02-09 15:02:11 +0000

    [ADD] 2 alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	int x;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
