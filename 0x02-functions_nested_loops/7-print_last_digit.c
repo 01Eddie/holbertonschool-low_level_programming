5fc62049e521a56ce5060f8233e312d3b1bcbb55
01Eddie <jesus.11.97@hotmail.com>
2021-02-09 22:50:57 +0000

    [ADD] 7 last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - Short description, single line
+ * @num: Description of parameter j
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: val
+ */
+int print_last_digit(int num)
+{
+	int val;
+	val = num % 10;
+
+	if (num < 0)
+	{
+		_putchar(-val + '0');
+		return (-val);
+	}
+	else
+	{
+		_putchar(val + '0');
+		return (val);
+	}
+}
