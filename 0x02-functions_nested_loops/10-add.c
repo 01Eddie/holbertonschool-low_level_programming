d223208f5d881e00fbb8bd8f705f1be0709a1aa3
01Eddie <jesus.11.97@hotmail.com>
2021-02-10 04:09:01 +0000

    [ADD] 10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - Short description, single line
+ * @a: First number
+ * @b: Second number
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
