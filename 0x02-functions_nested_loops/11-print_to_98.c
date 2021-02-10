4984af40b65ad51af7f315e014c6390aca72114a
01Eddie <jesus.11.97@hotmail.com>
2021-02-10 04:32:31 +0000

    [ADD] 11 print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Short description, single line
+ * @n: Parameter
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: no
+ */
+
+void print_to_98(int n)
+{
+	int count;
+
+	for (count = n; count < 99; count++)
+	{
+		if(count == 98)
+			printf("%d", count);
+		else
+			printf("%d, ", count);
+	}
+	printf("\n");
+}
