7584bbb03626286046977c221cc58cc604f9dea6
01Eddie <jesus.11.97@hotmail.com>
2021-02-11 19:01:32 +0000

    [ADD] 2 mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - Short description, single line
+ * @a: Description of parameter a
+ * @b: Description of parameter b
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: a * b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
