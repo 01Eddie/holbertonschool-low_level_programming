6f90be28d787ffffa3990aa821ae94dc081d87ba
01Eddie <jesus.11.97@hotmail.com>
2021-02-11 20:30:20 +0000

    [ADD] 3 print numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,15 @@
 #include "holberton.h"
 
 /**
- * print_numbers- short description, single line
+ * print_most_numbers- short description, single line
  * @void: description of parameter a
 (* a blank line
  * description: longer description of the function)?
 (* section header: section description)*
  * return: void
 */
-void print_numbers(void)
+
+void print_most_numbers(void)
 {
-	for (char number = '0'; number <= '9'; number++)
-	{
-		_putchar(number);
-	}
-	_putchar('\n');
+	
 }
