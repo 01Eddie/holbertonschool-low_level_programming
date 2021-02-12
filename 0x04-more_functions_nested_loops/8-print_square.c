f4d9a61fdae401937d66cf725ef961ec13c29819
01Eddie <jesus.11.97@hotmail.com>
2021-02-12 01:17:13 +0000

    [ADD] 8 print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - short description, single line
+ * @n: description of parameter a
+(* a blank line
+ * description: longer description of the function)?
+(* section header: section description)*
+ * return: void
+*/
+
+void print_square(int size)
+{
+	int fila, columna;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (fila = 0; fila < size; fila++)
+		{
+			for (columna = 0; columna < size; columna++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
