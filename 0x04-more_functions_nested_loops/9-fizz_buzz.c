8c8cdf00e6d538c23caf38b1558200d6a8854c72
01Eddie <jesus.11.97@hotmail.com>
2021-02-12 02:13:34 +0000

    [ADD] 9 fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,31 @@
+#include "holberton.h"
 #include <stdio.h>
 
 /**
- * print_square - short description, single line
- * @size: description of parameter a
+ * main - print Fizz Buzz of numbers 3 and 5
+ * @void: description of parameter a
 (* a blank line
- * description: longer description of the function)?
+ * description: main of numbers
 (* section header: section description)*
- * return: void
-*/
+ * return: Always 0
+ */
 
 int main(void)
 {
 	int i;
-	for(i = 1; i <= 100; i++)
+
+	for (i = 1; i <= 100; i++)
 	{
-		if(i%3 == 0)
-		{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
 			printf("Fizz ");
-		}
-		else if(i%5 == 0)
-		{
+		else if (i % 5 == 0)
 			printf("Buzz ");
-		}
 		else
-		{
 			printf("%d ", i);
-		}
 	}
 	printf("\n");
+
+	return (0);
 }
