ea8e39c63d56225b4ac6419942f483a97dbb21d3
01Eddie <jesus.11.97@hotmail.com>
2021-02-13 05:51:20 +0000

    [ADD] 100 print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main- Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (i == '8' && j == '9')
+			{
+				putchar(i);
+				putchar(j);
+				putchar(' ');
+			}
+			else if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
