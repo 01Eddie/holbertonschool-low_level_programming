6042b87a526fe38c18c6864c060a30fab58cfda6
01Eddie <jesus.11.97@hotmail.com>
2021-02-16 16:09:40 +0000

    [A]

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ * _strlen - short description, single line
+ * @s: description of parameter s
+(* a blank line
+ * description: longer description of the function)?
+(* section header: section description)*
+ * Return: void
+*/
+
+int _strlen(char *s)
+{
+	int count;
+
+	while (s[count] != '\n')
+		count++;
+
+	return (s[count]);
+}
