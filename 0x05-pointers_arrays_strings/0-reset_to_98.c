0b68d9beb8883d71a188cda3c2a0c830dc349943
01Eddie <jesus.11.97@hotmail.com>
2021-02-16 15:18:31 +0000

    [ADD] 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - short description, single line
+ * @n: description of parameter n
+(* a blank line
+ * description: longer description of the function)?
+(* section header: section description)*
+ * Return: void
+*/
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
