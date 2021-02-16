78535ede290bd39a007bd1b3eeb314533af28619
01Eddie <jesus.11.97@hotmail.com>
2021-02-16 16:42:26 +0000

    [ADD] 2 strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
-#include <string.h>
 /**
  * _strlen - short description, single line
  * @s: description of parameter s
 (* a blank line
  * description: longer description of the function)?
 (* section header: section description)*
- * Return: void
+ * Return: count 
 */
 
 int _strlen(char *s)
