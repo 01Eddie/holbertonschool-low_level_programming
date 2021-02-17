5bd0b55fed051e9c7f8aa7e3295b0c8de211fc87
01Eddie <jesus.11.97@hotmail.com>
2021-02-17 19:16:23 +0000

    [ADD] 9 strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,12 +14,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int count = 0;
 
-	while (*(src + count))
+	while (src[count] != '\0')
 	{
 		dest[count] = src[count];
 		count++;
 	}
+	dest[count] = '\0';
 
-
-	return (dest + '\0');
+	return (dest);
 }
