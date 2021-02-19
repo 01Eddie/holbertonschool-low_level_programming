56b9b439cd1e0a279768d19e4cf101a57807e68b
01Eddie <jesus.11.97@hotmail.com>
2021-02-19 00:21:05 +0000

    [ADD] 0 strcat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strncat - short description, single line
+ * @dest: character
+ * @src: character
+ * @n: integer
+ * Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	n = 0;
+	
+}
