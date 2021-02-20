ffd486b6aaf20745e3df33051ff5f6caf0a7bec0
01Eddie <jesus.11.97@hotmail.com>
2021-02-20 04:50:09 +0000

    [ADD] 7 leet

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -4,7 +4,6 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
-<<<<<<< HEAD
 char *cap_string(char *);
-=======
->>>>>>> a4ffc2f9220c97c4dafef63d8f35d29e6401342e
+char *leet(char *);
+char *rot13(char *);
