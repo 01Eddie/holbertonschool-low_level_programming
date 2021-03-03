cfe28fce08f134ba5c6b69c9b575ff4db3b9bf6f
01Eddie <jesus.11.97@hotmail.com>
2021-03-02 19:59:24 -0500

    [ADD] 1 strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,58 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _strdup - writes the character c to stdout
+ * @str: Character
+ * Return: NULL or ar
+ */
+
+char *_strdup(char *str)
+{
+	char *dest = malloc(_strlen(str) + 1);
+
+	if (dest == NULL)
+		return (NULL);
+
+	_strcpy(dest, str);
+
+	return (dest);
+}
+
+/**
+ * _strlen - short description, single line
+ * @s: description of parameter s
+ * Return: length of s
+*/
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+		count++;
+
+	return (count);
+}
+
+/**
+ * _strcpy - short description, single line
+ * @dest: character
+ * @src: character
+ * Return: dest + '\0'
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (src[count] != '\0')
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	dest[count] = '\0';
+
+	return (dest);
+}
