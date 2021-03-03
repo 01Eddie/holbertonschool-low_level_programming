a277b4d2d9c0df87496f7dec694167a0372b7a09
01Eddie <jesus.11.97@hotmail.com>
2021-03-03 00:16:03 -0500

    [ADD] 4 free grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,12 +1,19 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+/**
+ * free_grid- writes the character c to stdout
+ * @grid: Integer
+ * @height: Integer
+ * Return: void
+ */
+
 void free_grid(int **grid, int height)
 {
 	int i = 0;
 
 	for (; i < height; i++)
-		free(grid[i]);
+			free(grid[i]);
 
 	free(grid);
 }
