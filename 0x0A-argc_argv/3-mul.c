2c7a0579060469af0c3d24e68facc1f937c7e2a6
01Eddie <jesus.11.97@hotmail.com>
2021-03-03 21:27:04 -0500

    [ADD] 3 mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - writes the character c to stdout
+ * @argc: Integer
+ * @argv: Character
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	else if (argc < 4)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+
+	return (0);
+}
