e1639a547b9091ed316746eae8d35fb3143b9bd9
01Eddie <jesus.11.97@hotmail.com>
2021-03-11 18:14:32 -0500

    [ADD] README header and 0-sum_them_all

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int sum_them_all(const unsigned int n, ...);
+#endif /* _SAMPLE_HEADER_H_ */
