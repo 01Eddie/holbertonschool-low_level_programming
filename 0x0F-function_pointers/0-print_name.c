e3df54bfc74c6d7838388ef9a28823df4c7ec3fe
01Eddie <jesus.11.97@hotmail.com>
2021-03-11 04:01:49 -0500

    [modified] print name and [ADD] header library stdio

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * print_name - prints a name as is
  * @name: Character
@@ -9,5 +10,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+
 	(*f)(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,14 @@
 #ifndef _function_pointer_
 #define _function_pointer_
-
+#include <stdio.h>
 
 
 int _putchar(char c);
+
 void print_name(char *name, void (*f)(char *));
+
 void array_iterator(int *array, size_t size, void (*action)(int));
+
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* _function_pointer_ */
