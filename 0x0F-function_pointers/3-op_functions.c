4a956ae5aa3db1d4047eb270826caeb15be95f2c
01Eddie <jesus.11.97@hotmail.com>
2021-03-11 02:39:05 -0500

    [ADD] write program that performs simple operations (+, - ,*, /, %)

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Function in addition
+ * @a: Integer
+ * @b: Integer
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Function in subtration
+ * @a: Integer
+ * @b: Integer
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Function in multiplication
+ * @a: Integer
+ * @b: Integer
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Function in division
+ * @a: Integer
+ * @b: Integer
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_mod - Function in modulo
+ * @a: Integer
+ * @b: Integer
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
