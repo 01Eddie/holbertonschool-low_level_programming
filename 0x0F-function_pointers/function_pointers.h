f59319d15950243ad04ab4a0d4a19ce418c87147
01Eddie <jesus.11.97@hotmail.com>
2021-03-10 22:35:47 -0500

    [ADD] 2 int index

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,11 @@
 #ifndef _function_pointer_
 #define _function_pointer_
 
-int _putchar(char c);
 
+
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
 #endif /* _function_pointer_ */
