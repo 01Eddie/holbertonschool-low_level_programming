2c066b1dddfa7828c94fc2fac3765581963e656c
01Eddie <jesus.11.97@hotmail.com>
2021-03-14 18:05:16 -0500

    [ADD] 2 strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _strlen_recursion - recursion function.
+ * @s: Pointer of Character
+ * Return: (_strlen_recursion(s+1)+1) or (0).
  */
 
 int _strlen_recursion(char *s)
