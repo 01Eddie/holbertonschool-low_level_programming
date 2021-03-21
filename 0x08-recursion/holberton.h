4213c7242b4cc74a0a018f449f20df50f3387417
01Eddie <jesus.11.97@hotmail.com>
2021-03-21 01:06:51 -0500

    [modified] 7-is_palidrome.c

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,3 +1,5 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
 void _puts_recursion(char *s);
 int _putchar(char c);
@@ -13,3 +15,5 @@ int palindrome(char *c, int i, int f);
 int _strlen(char *s);
 int _sqrt(int val, int aux);
 int is_prime(int num, int div);
+
+#endif /* _HOLBERTON_H_ */
