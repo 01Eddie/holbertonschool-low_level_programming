2f60c2c18c81314230b5deea6be085badd86569e
01Eddie <jesus.11.97@hotmail.com>
2021-03-21 00:56:23 -0500

    [ADD] 7-is_palindrome.c

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -7,5 +7,9 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
 
+int palindrome(char *c, int i, int f);
+int _strlen(char *s);
 int _sqrt(int val, int aux);
+int is_prime(int num, int div);
