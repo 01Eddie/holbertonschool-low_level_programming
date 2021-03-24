98f0452c9a85a199df8acb8e4b231a9d881e6637
01Eddie <jesus.11.97@hotmail.com>
2021-03-23 21:30:37 -0500

    [ADD] 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * pop_listint - check the code for Holberton School students.
+ * @head: listint_t
+ * Return: 0 or result.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	/*underflow condition*/
+	if (*head == NULL)
+		return (0);
+
+	node = *head;
+	/*Pull out data before the node is deleted*/
+	int result = node->n;
+/*unlink the head node for the caller*/
+	*head = node->next;
+/* Note the `*` — uses a reference-pointer*/
+/*just like `push()` and `deleteList()`.*/
+
+/*free the head node*/
+	free(node);
+	/*don't forget to return the data*/
+	return (result);
+}
