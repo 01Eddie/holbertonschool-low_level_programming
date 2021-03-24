422b7d43760a1e24b592983cd1a09102be0fc045
01Eddie <jesus.11.97@hotmail.com>
2021-03-24 01:36:02 -0500

    [ADD] 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,26 +1,44 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * delete_nodeint_at_index - check the code for Holberton School students.
+ * @head: listint_t
+ * @index: unsigned integer
+ * Return: 1 or -1.
  */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int count;
 	/*store head node*/
-	listint_t *temp = *head;
-	listint_t *prev;
+	listint_t *temp;
+	listint_t *next;
 
+/*If key(index) wasn't present in linked list*/
+	if (head == NULL || *head == NULL)
+		return (-1);
 	/*If head node itself holds the key to be deleted*/
-	if (temp != NULL && temp->data == index)
+    /* && temp != NULL && temp->n == index */
+	if (index == 0)
 	{
-		*head = temp->next;/*Changed head*/
-		free(temp);/*free old head*/
-		return 1;
+		next = (*head)->next;/*Changed head*/
+		free(*head);/*free old head*/
+		*head = next;
+		return (1);
 	}
-
+	temp = *head;
 	/*Search for the key(index) to be deleted, keep track of the previous node*/
 	/*as we need to change 'prev->next'*/
-	while (temp != NULL && temp->data)
+	for (count = 0; count < index - 1; count++)
+	{
+		if (temp->next == NULL)
+			return (-1);
+		temp = temp->next;
+	}
+	next = temp->next;
+	/*Unlink the node from linked list*/
+	temp->next = next->next;
+	free(next);/*free memory*/
+
+	return (1);
 }
