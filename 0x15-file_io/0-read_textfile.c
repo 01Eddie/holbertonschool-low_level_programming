09f381ef67a8944e4d270fa709243f024f5df96e
01Eddie <jesus.11.97@hotmail.com>
2021-03-31 00:11:56 -0500

    [ADD] 0-read_textfile.c 1-create_file.c 2-append_text_to_file.c holberton.h and README.md

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,4 @@
 #include "holberton.h"
-#include <unistd.h>
-
 /**
  * read_textfile - check the code for Holberton School students.
  * @filename: Character
@@ -39,17 +37,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(fd);
 
 	return (countLetter);
-}
-
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
