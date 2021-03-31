82105c2aabedbb81f6b217fdf8d7e16408bedb36
01Eddie <jesus.11.97@hotmail.com>
2021-03-31 00:39:53 -0500

    [modified] 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,35 +7,39 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int wr, fd;
-	char *buff;
-	size_t countLetter = 0;
+	int nb, fd;/*nb: number of bytes*/
+	char *buffer;
+	/* size_t countLetter = 0;  */
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY, 2);
+	if (letters == 0)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
 
-	buff = malloc(sizeof(char) * letters);
-	wr = read(fd, buff, letters);
+	buffer = malloc(sizeof(char) * letters);
+	nb = read(fd, buffer, letters);
 
-	while (countLetter <= letters && buff[countLetter] != '\0')
-	{
-		wr = _putchar(buff[countLetter]);
-		countLetter++;
+	/*while (countLetter <= letters && buffer[countLetter] != '\0')*/
+	/*{*/
+/*		nb = _putchar(buffer[countLetter]);*/
+/*		countLetter++;*/
 
-		if (wr == -1)
+		if (nb == -1)
 		{
 			close(fd);
 			return (0);
 		}
-	}
+/*	}*/
+		write(STDOUT_FILENO, buffer, nb);
 
-	free(buff);
+	free(buffer);
 	close(fd);
 
-	return (countLetter);
+	return (nb);
 }
