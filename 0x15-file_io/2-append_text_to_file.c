a99375f82189aa90a3a9ef5952b22e23008acf49
01Eddie <jesus.11.97@hotmail.com>
2021-03-31 01:26:10 -0500

    [modified] 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		while (*text_content != '\0')
 		{
-			write(fd, text_content, text_content++);
+			write(fd, text_content, 1);
+			text_content++;
 		}
 	}
 
