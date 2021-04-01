67cb7fb05df255db523114783e708e42c31cf4a6
01Eddie <jesus.11.97@hotmail.com>
2021-04-01 00:33:18 -0500

    [ADD] 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,4 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  * main - Principal function
