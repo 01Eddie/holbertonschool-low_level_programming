5817a7cfd9e291fe6f3cc162d323776b7969ef7b
01Eddie <jesus.11.97@hotmail.com>
2021-04-22 19:49:47 -0500

    [ADD] 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned int count;
+	unsigned long int a = 1, b = 1, c;
+
+	printf("%lu, ", a);
+
+	for (count = 0; count <= 96; count++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+
+		if (count != 96)
+			printf("%lu, ", b);
+		else
+			printf("%lu\n", b);
+	}
+	return (0);
+}
