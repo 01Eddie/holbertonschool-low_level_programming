863d5d8c5ef1295d3741feb829fa60f989060b9e
01Eddie <jesus.11.97@hotmail.com>
2021-04-22 23:12:44 -0500

    [ADD] 100-atoi.c [modified] holberton.h

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+/**
+ * _atoi - function that convert a string to an integer
+ * @s: Character string pointer
+ *
+ * Return: number * N
+ */
+int _atoi(char *s)
+{
+	int count1 = 0, n = -1, number = 0, isNum = 0;
+
+	while (s[count1] != '\0')
+	{
+		if (s[count1] == '-')
+			n = -n;
+		if (s[count1] >= '0' && s[count1] <= '9')
+		{
+			number = (number * 10) - (s[count1] - '0');
+			isNum = 1;
+		}
+		else if (isNum == 1)
+			break;
+		count1++;
+	}
+	return (number * n);
+}
