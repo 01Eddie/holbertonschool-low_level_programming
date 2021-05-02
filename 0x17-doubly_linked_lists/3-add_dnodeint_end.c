1daa88e2cf1eb032512f147b5bd3c5d9e681b1a8
01Eddie <jesus.11.97@hotmail.com>
2021-05-02 16:20:51 -0500

    [ADD] 3-add_dnodeint_end.c lists.h

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - check the code for Holberton School students.
+ * @head: dlistint_t
+ * @n: constant integer
+ * Return: NULL or new_node
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	dlistint_t *last = *head;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if ((*head) == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return NULL;
+	}
+
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = new_node;
+	new_node->prev = last;
+	return (new_node);
+}
