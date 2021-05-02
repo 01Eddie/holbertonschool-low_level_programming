e80d60e44f24c1d27ecff86b493beda02160785a
01Eddie <jesus.11.97@hotmail.com>
2021-05-02 18:18:50 -0500

    [ADD] 5-get_dnodeint.c lists.h

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,7 +14,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
-	
+
+    if (index == 0)
+		return (head);
+
 	while (get_node != NULL)
 	{
 		if (i == index)
