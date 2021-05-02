f94b01e881ce2c58f902e7920f2f8d80b901e367
01Eddie <jesus.11.97@hotmail.com>
2021-05-02 13:11:58 -0500

    [ADD] 2-add_dnodeint.c lists.h

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - check the code for Holberton School students.
+ * @head: dlistint double pointer
+ * @n: constant integer
+ * Return: NULL or *head
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	new_node->n = n;
+	new_node->next = (*head);
+	new_node->prev = NULL;
+
+
+	if ((*head) != NULL)
+		(*head)->prev = new_node;
+	(*head) = new_node;
+
+	if ((*head) == NULL)
+		return (NULL);
+
+	return (*head);
+}
