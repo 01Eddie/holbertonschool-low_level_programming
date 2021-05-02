06736708bb3d564d5e7dfc212334e9dfd692d418
01Eddie <jesus.11.97@hotmail.com>
2021-05-02 16:29:27 -0500

    [ADD] 3-add_dnodeint_end.c lists.h

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
-    if ((*head) == NULL)
+	if ((*head) == NULL)
 	{
 		new_node->prev = NULL;
 		*head = new_node;
