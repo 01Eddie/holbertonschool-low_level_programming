093693dce8ecd42180ac84d743bd781afe1ad6e0
01Eddie <jesus.11.97@hotmail.com>
2021-05-02 16:00:43 -0500

    [ADD] 2-add_dnodeint.c lists.h

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,11 +14,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = (*head);
 	new_node->prev = NULL;
-
 	(*head) = new_node;
 
 	if (new_node == NULL)
 		return (NULL);
+
+	if ((*head) != NULL)
+		(*head)->prev = new_node;
+	
+
+	
 
 	return (new_node);
 }
