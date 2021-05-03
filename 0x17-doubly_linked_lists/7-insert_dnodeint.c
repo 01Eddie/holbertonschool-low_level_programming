3be6bce9c40f64fba05fba8f1cb584c82b5a2540
01Eddie <jesus.11.97@hotmail.com>
2021-05-02 21:07:27 -0500

    [ADD] 7-insert_dnodeint.c lists.h

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,47 +5,45 @@
  * @h: pointer of dlistint_t
  * @idx: unsigned integer
  * @n: integer
- * Return: new_node_node or NULL
+ * Return: new_node or NULL
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *last;
-	dlistint_t *new_node;
 	unsigned int i;
-
-	last = *h;
+	dlistint_t *new_node, *node, *next;
 
 	if (h == NULL)
 		return (NULL);
 	if (idx != 0)
 	{
-		for (i = 0; i < idx - 1 && last != NULL; i++)
-			last = last->next;
+		i = 0;
+		while (i < idx - 1 && node != NULL)
+		{
+			node = node->next;
+			i++;
+		}
 	}
 	if (idx > i + 1)
 		return (NULL);
-
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
 	if (idx == 0)
 	{
-		new_node->next = *h;
-		new_node->prev = NULL;
+		new_node->next = *h, new_node->prev = NULL;
 		*h = new_node;
 		return (new_node);
 	}
 	if (idx == i)
 	{
-		new_node->prev = last;
+		new_node->prev = node;
 		new_node->next = NULL;
-		last->next = new_node;
+		node->next = new_node;
 		return (new_node);
 	}
-	new_node->prev = last;
-	new_node->next = last->next;
-	last->next = new_node;
+	new_node->prev = node;
+	new_node->next = node->next;
+	node->next = new_node;
 	return (new_node);
 }
