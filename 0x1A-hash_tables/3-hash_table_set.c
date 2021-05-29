914756b5586ec00dc00dd12a4190e2545cbeb2a6
01Eddie <jesus.11.97@hotmail.com>
2021-05-28 23:29:49 -0500

    [ADD] all files

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,9 +18,40 @@
 // In case of collision, add the new node at the beginning of the list
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	if (ht == NULL)
+	hash_node_t *node = NULL, *oldhead, *tmp;
+	unsigned long int hash_index;
+
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	char index_key = hash_djb2(key);
-	key->next = value;
+	if (strlen(key) < 1)
+		return (0);
+	node = malloc(sizeof(hash_node_t));
+	if(node == NULL)
+		return (0);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
+
+	hash_index = key_index((const unsigned char *)key, ht->size);
+	/* verified if value is NULL. If not make linked list*/
+	if (ht->array[hash_index] == NULL)
+		ht->array[hash_index] = node;
+	else
+	{
+		oldhead = ht->array[hash_index];
+		for (tmp = oldhead; tmp != NULL; tmp = tmp->next)
+		{
+			if (strcmp(key, tmp->key) == 0)
+			{
+				free(tmp->value);
+				tmp->value = strdup(value);
+				free(node->value);
+				free(node);
+				return (1);
+			}
+		}
+		node->next = oldhead;
+		ht->array[hash_index] = node;
+	}
 	return (1);
 }
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function that retrieves a value associated with a key.
+ * ht: constant hash_table
+ * Return: NULL or element
+ */
+
+// Write a function that retrieves a value associated with a key.
+// 
+// Prototype: char *hash_table_get(const hash_table_t *ht, const char *key);
+// where ht is the hash table you want to look into
+// and key is the key you are looking for
+// Returns the value associated with the element, or NULL if key couldn’t be found
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    hash_table_t element;
+    
+}
