6ee0a68ca5efa31c3583e6dec6ba43a5423cfa7d
01Eddie <jesus.11.97@hotmail.com>
2021-07-03 03:18:06 -0500

    [ADD] 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,22 +14,17 @@ def island_perimeter(grid):
 
     for line in grid:
         c = 0
-
         for val in line:
             if val == 1:
                 surround = 4
-                if c != length - 1:
-                    if grid[d][c + 1] == 1:
-                        surround -= 1
-                if c != 0:
-                    if grid[d][c - 1] == 1:
-                        surround -= 1
-                if d != height - 1:
-                    if grid[d + 1][c] == 1:
-                        surround -= 1
-                if d != 0:
-                    if grid[d - 1][c] == 1:
-                        surround -= 1
+                if c != length - 1 and grid[d][c + 1] == 1:
+                    surround -= 1
+                if c != 0 and grid[d][c - 1] == 1:
+                    surround -= 1
+                if d != height - 1 and grid[d + 1][c] == 1:
+                    surround -= 1
+                if d != 0 and grid[d - 1][c] == 1:
+                    surround -= 1
                 perimeter += surround
             c += 1
         d += 1
