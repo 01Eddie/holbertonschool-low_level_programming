e1f315dbb04be2636c69e4ffaa5b237d8c056e46
01Eddie <jesus.11.97@hotmail.com>
2021-08-30 23:36:02 -0500

    [ADD] task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -44,23 +44,23 @@ int binary_search(int *array, size_t size, int value)
 			* return m
 		* return unsuccesful
 	 */
-	if (array && size > 0)
-	{
-		size_t l = 0, m;
-		size_t r = size - 1;
+	size_t l = 0, m;
+	size_t r = size - 1;
 
+	if (!array)
+		return (-1);
+
+	print_array(array + l, r + 1 - l);
+	while (l < r)
+	{
+		m = (l + r) / 2;
+		if (array[m] < value)
+			l = m + 1;
+		else if (array[m] > value)
+			r = m - 1;
+		else
+			return (m);
 		print_array(array + l, r + 1 - l);
-		while (l < r)
-		{
-			m = (l + r) / 2;
-			if (array[m] < value)
-				l = m + 1;
-			else if (array[m] > value)
-				r = m - 1;
-			else
-				return (m);
-			print_array(array + l, r + 1 - l);
-		}
 	}
 	return (-1);
 }
