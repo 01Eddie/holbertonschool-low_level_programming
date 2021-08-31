7cee69142bbc0233e914969e0b0e4c64e5b89fa0
01Eddie <jesus.11.97@hotmail.com>
2021-08-30 23:11:16 -0500

    [ADD] task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,64 @@
+#include "search_algos.h"
+/*
+ * printArray - Function
+ * @a: Integer
+ * @n: Size_t
+ * Return: Void
+ */
+void print_array(int *a, size_t n)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+			printf(", ");
+	}
+
+	printf("\n");		
+}
+
+/**
+ * binary_search - function
+ * @array: Pointer of Integer
+ * @size: Size_t
+ * @value: Integer
+ * Return:
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	/*
+function binary_search(A, n, T) is
+    L := 0
+    R := n − 1
+    while L ≤ R do
+        m := floor((L + R) / 2)
+			 if A[m] < T then
+            L := m + 1
+        else if A[m] > T then
+            R := m − 1
+        else:
+            return m
+		return unsuccesful
+	 */
+	if (array == NULL && size < 1)
+		return (-1);
+	size_t l = 0, m;
+	size_t r = size - 1;
+	print_array(array + l, r + 1 - l);
+	while (l < r)
+	{
+		m = (l+r) / 2;
+		if (array[m] < value)
+			l = m + 1;
+		else if (array[m] > value)
+			r = m - 1;
+		else
+			return (m);
+		print_array(array + l, r + 1 - l);
+	}
+	return (-1);
+}
